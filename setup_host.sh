5e3c01f24d9a61ca119b9ebe46d2d7d101671c09
Jonn Smith <11667487+jonn-smith@users.noreply.github.com>
2026-05-06 17:23:48 +0000

    Updated setup script.Updated setup script.Updated setup script.Updated setup script.Updated setup script.Updated setup script.Updated setup script.Updated setup script.

--- setup_host.sh
+++ setup_host.sh
@@ -1,10 +1,44 @@
 #!/usr/bin/env bash
 
+################################################################################
+
 set -euo pipefail
+
+################################################################################
+
+echo "Installing docker"
+# Add Docker's official GPG key:
+sudo apt update
+sudo apt install -y ca-certificates curl
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+# Add the repository to Apt sources:
+sudo tee /etc/apt/sources.list.d/docker.sources <<EOF
+Types: deb
+URIs: https://download.docker.com/linux/ubuntu
+Suites: $(. /etc/os-release && echo "${UBUNTU_CODENAME:-$VERSION_CODENAME}")
+Components: stable
+Architectures: $(dpkg --print-architecture)
+Signed-By: /etc/apt/keyrings/docker.asc
+EOF
+
+sudo apt update
+
+sudo apt install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+sudo systemctl start docker
+
+echo "Adding current user: ${USER} to docker group"
+getent group docker || sudo groupadd docker
+sudo usermod -aG docker $USER
+newgrp docker
+
+################################################################################
 
 echo "Installing sysbox for docker-in-a-docker support"
 wget https://downloads.nestybox.com/sysbox/releases/v0.6.7/sysbox-ce_0.6.7-0.linux_amd64.deb
-sudo apt install ./sysbox-ce_0.6.7-0.linux_amd64.deb
+sudo apt install -y ./sysbox-ce_0.6.7-0.linux_amd64.deb
 # postinst restarts dockerd, registers sysbox-runc in /etc/docker/daemon.json
 
 #Verify:
@@ -16,6 +50,7 @@ docker info | grep -i runtime
 # Must setup the postconf to include all subnets from docker images to get
 # mail to forward properly:
 # mynetworks = 127.0.0.0/8 10.0.0.0/8 172.16.0.0/12 192.168.0.0/16
+sudo DEBIAN_FRONTEND=noninteractive apt install -y postfix
 echo "Updating postfix config file to use all local networks to enable email forwarding from inside the docker image."
 echo -n "Previous value: "
 postconf -h mynetworks
